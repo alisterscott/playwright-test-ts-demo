53803bd601ea748290767561176cff4861e67ca7
Alister Scott <alister.scott@gmail.com>
2021-10-14 12:09:48 +1000

    try playwright browser image

--- scenarios/example01.spec.ts
+++ scenarios/example01.spec.ts
@@ -4,12 +4,10 @@ import { visitHomePage } from '../lib/actions/nav'
 test('can wait for an element to appear', async ({ page }) => {
   await visitHomePage(page)
   await page.waitForSelector('#elementappearschild', { state: 'visible', timeout: 5000 })
-  expect(await page.screenshot()).toMatchSnapshot('element-appears.png', { threshold: 0.5 })
 })
 
 test('can use an element that appears after on page load', async ({ page }) => {
   await visitHomePage(page)
   const text = await page.textContent('#loadedchild')
   expect(text).toBe('Loaded!')
-  expect(await page.screenshot()).toMatchSnapshot('element-after-onload.png', { threshold: 0.5 })
 })
