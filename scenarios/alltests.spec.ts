f1f26c419d617597450f06aa7a7dd64d2cd5b1b5
Alister Scott <alister.scott@gmail.com>
2021-10-22 17:01:22 +1000

    Try API testing in playwright (#3)

--- scenarios/alltests.spec.ts
+++ scenarios/alltests.spec.ts
@@ -38,4 +38,39 @@ test.describe.parallel('All tests', () => {
     await goToPath(page, 'error')
     expect(errors).toBe('Purple Monkey Dishwasher Error')
   })
+
+  test('can GET a REST API and check response using approval style', async ({ request }) => {
+    const response = await request.get('https://my-json-server.typicode.com/webdriverjsdemo/webdriverjsdemo.github.io/posts')
+    expect(response.status()).toBe(200)
+    const body = await response.text()
+    expect(body).toMatchSnapshot('posts.txt')
+  })
+
+  test('can GET a REST API and check response using assertion style', async ({ request }) => {
+    const response = await request.get('https://my-json-server.typicode.com/webdriverjsdemo/webdriverjsdemo.github.io/posts')
+    expect(response.status()).toBe(200)
+    const body = JSON.parse(await response.text())
+    expect(body.length).toBe(3)
+    expect(body[0].id).toBe(1)
+    expect(body[0].title).toBe('Post 1')
+    expect(body[1].id).toBe(2)
+    expect(body[1].title).toBe('Post 2')
+    expect(body[2].id).toBe(3)
+    expect(body[2].title).toBe('Post 3')
+  })
+
+  test('can POST a REST API and check response using approval style', async ({ request }) => {
+    const response = await request.post('https://my-json-server.typicode.com/webdriverjsdemo/webdriverjsdemo.github.io/posts', { data: { title: 'Post 4' } })
+    expect(response.status()).toBe(201)
+    const body = await response.text()
+    expect(body).toMatchSnapshot('post4.txt')
+  })
+
+  test('can POST a REST API and check response using assertion style (using page)', async ({ page }) => {
+    const response = await page.request.post('https://my-json-server.typicode.com/webdriverjsdemo/webdriverjsdemo.github.io/posts', { data: { title: 'Post 4' } })
+    expect(response.status()).toBe(201)
+    const body = JSON.parse(await response.text())
+    expect(body.id).toBe(4)
+    expect(body.title).toBe('Post 4')
+  })
 })
