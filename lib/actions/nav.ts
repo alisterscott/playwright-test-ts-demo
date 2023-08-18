493cff9f824ec5e6ab40cfe9f6b0c6bfac6cc3b6
Alister Scott <alister.scott@translink.com.au>
2023-08-18 13:04:58 +1000

    add winston

--- lib/actions/nav.ts
+++ lib/actions/nav.ts
@@ -1,12 +1,33 @@
 import { Page } from '@playwright/test'
 import config from 'config'
+import { logging } from '../logging'
+
+function escDoubleQuoteChars (phrase: string) {
+  return phrase.replace('"', '""')
+}
+
+async function logPageLoadTimes (page: Page) {
+  const navTimings = await page.evaluate(() =>
+    performance.getEntriesByType('navigation')
+  )
+  for (const nt of navTimings) {
+    let CSVStr = ''
+    for (const key in nt) {
+      // CSVStr += `"${escDoubleQuoteChars(key.toString())}",` // use this to add the headers
+      CSVStr += `"${escDoubleQuoteChars(nt[key].toString())}",`
+    }
+    logging.info(CSVStr)
+  }
+}
 
 export async function visitHomePage (page: Page) {
-  return await page.goto(`${config.get('baseURL')}`)
+  await page.goto(`${config.get('baseURL')}`)
+  await logPageLoadTimes(page)
 }
 
 export async function goToPath (page: Page, path: string) {
-  return await page.goto(`${config.get('baseURL')}/${path}`)
+  await page.goto(`${config.get('baseURL')}/${path}`)
+  await logPageLoadTimes(page)
 }
 
 export async function clickAndWait (page: Page, locator: string, expectResponseURL: string) {
