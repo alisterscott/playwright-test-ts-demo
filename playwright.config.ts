bf5eafee3f19be557ba325af6bf37159ce35ae27
Alister Scott <alister.scott@gmail.com>
2024-10-24 05:11:19 +0000

    update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,31 @@
-// playwright.config.ts
-module.exports = {
-  globalSetup: require.resolve('./global-setup'),
-  reporter: [['list'], ['html']],
-  retries: 0,
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  // Look for test files in the "tests" directory, relative to this configuration file.
+  testDir: 'scenarios',
+  globalSetup: './global-setup.ts',
+
+  // Run all tests in parallel.
+  fullyParallel: true,
+
+  // Fail the build on CI if you accidentally left test.only in the source code.
+  forbidOnly: !!process.env.CI,
+
+  // Retry on CI only.
+  retries: process.env.CI ? 2 : 0,
+
+  // Reporter to use
+  reporter: 'html',
+
   use: {
-    headless: true,
-    screenshot: 'only-on-failure',
-    video: 'retry-with-video',
-    trace: 'on'
-  }
-}
+    // Collect trace when retrying the failed test.
+    trace: 'on-first-retry',
+  },
+  // Configure projects for major browsers.
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ]
+});
